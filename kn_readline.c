0e5950607b194e235cb6563131cde04e7619ed6d
ntk221 <nittakdub@gmail.com>
2023-02-15 22:37:41 +0900

    マップとビルトイン周りを追加

---
+++ kn_readline.c
@@ -0,0 +1,270 @@
+#include "minishell.h"
+#include <stdio.h>
+
+#ifdef LEAKS
+__attribute__((destructor))
+static void destructor()
+{
+	system("leaks -q minishell");
+}
+#endif
+
+
+t_map	*g_env;
+
+
+void	shell_works(char *line)
+{
+	t_token	*tok;
+	t_node	*node;
+
+	tok = tokenizer(line);
+	expand(tok);
+	node = parse(tok);
+	exec(&node);
+	if (tok != NULL)
+		free_token(tok);
+	if (node != NULL)
+		free_command_node(node);
+}
+
+void  sigint_handler()
+{
+  // printf("%s\n", rl_line_buffer);
+  write(1, "\n", 1);
+  rl_on_new_line();
+  rl_replace_line("", 0); // buffer を 空にする
+  rl_redisplay(); // buffer を 表示する
+}
+
+#include <string.h>
+char  *get_name(char *name_and_value)
+{
+	size_t	len;
+	size_t	j;
+	char	*name;
+
+	len = 0;
+	while(name_and_value[len] != '\0')
+	{
+		if (name_and_value[len] == '=')
+			break;
+		len++;
+	}
+	name = malloc(sizeof(char) * (len + 1));
+	// error
+	j = 0;
+	while (len)
+	{
+		name[j] = name_and_value[j];
+		j++;
+		len--;
+	}
+	name[j] = '\0';
+	return (name);
+}
+
+void	env_init(t_map **env, char **envp)
+{
+	size_t	i;
+	char	*name;
+	char	*value;
+
+	i = 0;
+	*env = malloc(sizeof(t_map));
+	// error
+	while (envp[i] != NULL)
+	{
+		name = get_name(envp[i]);
+		value = getenv(name);
+		map_set(env, name, value);
+		i++;
+	}
+}
+
+bool  is_builtin(char *line)
+{
+  char		*set[] = {"export", "env", "pwd", "cd", "exit", "unset", "echo", NULL};
+  size_t	i;
+
+  i = 0;
+  while (set[i])
+  {
+	if (strncmp(set[i], line, ft_strlen(set[i])) == 0)
+	{
+		puts("found!");
+		return true;
+	}
+    i++;
+  }
+    return false;
+}
+
+void	ms_env(void)
+{
+	t_item	*itr;
+
+	itr = g_env->item_head;
+	while (itr != NULL)
+	{
+		size_t	size;
+		char	*name_and_value;
+
+		size = ft_strlen(itr->name) + ft_strlen(itr->value) + 2;
+		name_and_value = malloc(size);
+		strlcat(name_and_value, itr->name, size);
+		strlcat(name_and_value, "=", size);
+		strlcat(name_and_value, itr->value, size);
+		printf("%s\n", name_and_value);
+		free(name_and_value);
+		itr = itr->next;
+	}
+}
+
+void	ms_export(char *line)
+{
+	char	**commands;
+	char	**name_and_value;
+
+	commands = ft_split(line, ' ');
+	if (commands != 0 && commands[0] != NULL)
+	{
+		name_and_value = ft_split(commands[1], '=');
+		if (name_and_value[0] && name_and_value[1])
+		{
+			map_set(&g_env, name_and_value[0], name_and_value[1]);
+			return ;
+		}
+		else
+		{
+			// free
+			return ;
+		}
+	}
+	puts("TODO: print usage");
+	// free
+}
+
+void	ms_pwd(void)
+{
+	printf("%s\n", map_get(g_env, "PWD"));
+}
+
+void	ms_cd(char *line)
+{
+	char	**commands;
+	char	*path;
+
+	commands = ft_split(line, ' ');
+	// TODO: ~ を解釈する
+	if (commands == 0 || commands[1] == NULL)
+	{
+		puts("TODO: print usage");
+		return ;
+	}
+	path = commands[1];
+
+	if (chdir(path) < 0)
+	{
+		perror("chdir");
+		return ;
+	}
+
+	char	buf[256];
+	map_set(&g_env, "PWD", getcwd(buf, sizeof(buf)));
+	// free
+}
+
+void	ms_exit(char *line)
+{
+	char	**commands;
+
+	commands = ft_split(line, ' ');
+	if (commands[1] == NULL)
+	{
+		printf("exit\n");
+		exit(0);
+	}
+	if (!atoi(commands[1]) || (atoi(commands[1]) == -1 && ft_strlen(commands[1]) > 2))
+	{
+		printf("minishell: exit: numeric argument required\n");
+		exit(255);
+	}
+	printf("exit\n");
+	exit(atoi(commands[1]));
+	
+}
+
+void	ms_unset(char *line)
+{
+	char	**commands;
+
+	commands = ft_split(line, ' ');
+	if (commands == NULL || commands[1] == NULL)
+		return ;
+	map_unset(&g_env, commands[1]);
+}
+
+void	ms_echo(char *line)
+{
+	char	**commands;
+
+	commands = ft_split(line, ' ');
+	if (commands == NULL || commands[1] == NULL)
+		return ;
+	if (strcmp(commands[1], "-n") == 0)
+		ft_putstr_fd(commands[2], 1);
+	else
+		printf("%s\n", commands[1]);
+}
+
+void	do_builtin(char *line)
+{
+	if (strcmp(line, "env") == 0)
+		ms_env();
+	else if (strncmp(line, "export", 6) == 0)
+		ms_export(line);
+	else if (strcmp(line, "pwd") == 0)
+		ms_pwd();
+	else if (strncmp(line, "cd", 2) == 0)
+		ms_cd(line);
+	else if (strncmp(line, "exit", 4) == 0)
+		ms_exit(line);
+	else if (strncmp(line, "unset", 5) == 0)
+		ms_unset(line);
+	else if (strncmp(line, "echo", 4) == 0)
+		ms_echo(line);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	char	  *line;
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+
+	env_init(&g_env, envp);
+
+	rl_outstream = stderr;
+	if (argc > 2 && strncmp("-c", argv[1], 3) == 0)
+		shell_works(argv[2]);
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (line == NULL)
+		{
+			puts("exit");
+			break;
+		}
+		line = ft_strtrim(line, " ");
+		if (*line)
+			add_history(line);
+		if (line[0] == '/' || line[0] == '.')
+			abusolute_path(line);
+		else if (is_builtin(line))
+			do_builtin(line);
+		else
+			shell_works(line);
+		free(line);
+	}
+	exit(0);
+}
---
+++ map/map.c
@@ -0,0 +1,192 @@
+#include "../include/map.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief キーと値を受け取って，その組を返す.  
+ * @return t_item* 
+ */
+
+t_item	*item_new(char *name, char *value)
+{
+	t_item *item;
+	item = malloc(sizeof(t_item));
+	if (!item)
+	    return (NULL);
+	item->name = strdup(name);
+	item->value = strdup(value);
+	item->next = NULL;
+	return (item);
+}
+
+void	append_item(t_item **itr, t_item *item);
+
+t_map	*map_new(void)
+{
+	t_map	*map;
+
+	map = malloc(sizeof(t_map));
+	if (!map)
+	    return (NULL);
+	map->item_head = NULL;
+	return (map);
+}
+
+/**
+ * @brief : map に 新しいキーと値の組みを登録する. すでに登録されているキーの時は,更新する
+ * 
+ * @param t_map **map 
+ * @param char *name 
+ * @param char *value 
+ */
+
+void	map_set(t_map **map, char *name, char *value)
+{
+	t_item	*itr;
+	t_item	*item;
+
+	itr = (*map)->item_head;
+	while (itr != NULL)
+	{
+		if (strcmp(itr->name, name) == 0)
+		{
+			free(itr->value);
+			itr->value = strdup(value);
+			return ;
+		}
+		itr = itr->next;
+	}
+	item = item_new(name, value);
+	append_item(&(*map)->item_head, item);
+}
+
+/**
+ * @brief map に登録されている item を削除する
+ * 
+ * @param t_map **map 
+ * @param char *name 
+ */
+
+#include <stdio.h>
+void	map_unset(t_map **map, char *name)
+{
+	t_item	*prev;
+	t_item	*itr;
+
+	prev = (*map)->item_head;
+	itr = prev->next;
+	if (strcmp(prev->name, name) == 0)
+	{
+		free(prev->name);
+		free(prev->value);
+		free(prev);
+		(*map)->item_head = itr;
+	}
+	while (itr)
+	{
+		if (strcmp(itr->name, name) == 0)
+		{
+			free(itr->name);
+			free(itr->value);
+			prev->next = itr->next;
+			free(itr);
+			return ;
+		}
+		prev = itr;
+		itr = itr->next;
+	}
+}
+
+char    *map_get(t_map *map, const char *name)
+{
+    t_item  *itr;
+
+    itr = map->item_head;
+    while (itr != NULL)
+    {
+        if (strcmp(itr->name, name) == 0)
+            return (itr->value);
+        itr = itr->next;
+    }
+    return (NULL);
+}
+
+#include <stdio.h>
+void	append_item(t_item **target, t_item *item)
+{
+	if ((*target) == NULL)
+	{
+		(*target) = item;
+		return ;
+	}
+
+	append_item(&(*target)->next, item);
+}
+
+#ifdef TEST
+
+#include <assert.h>
+void	test_item(void)
+{
+	t_item *target = item_new("hoge", "huga");
+
+	t_item *item2 = item_new("hello", "world");
+	append_item(&target, item2);
+	assert(strcmp(target->name, "hoge") == 0);
+	assert(strcmp(target->next->name, "hello") == 0);
+
+	t_item *item3 = item_new("FOO", "BAR");
+	append_item(&target, item3);
+	assert(strcmp(target->next->next->name, "FOO") == 0);
+	printf("%s\n", target->next->next->name);
+}
+
+int main(void)
+{
+    t_item  *item = item_new("hoge", "huga");
+    assert(strcmp(item->name, "hoge") == 0);
+    assert(strcmp(item->value, "huga") == 0);
+    t_map *map = map_new();
+    assert(map->item_head == NULL);
+    map_set(&map, "hoge", "huga");
+    assert(strcmp(map->item_head->name, "hoge") == 0);
+    map_set(&map, "hello", "world");
+    assert(strcmp(map->item_head->name, "hoge") == 0);
+    assert(strcmp(map->item_head->next->name, "hello") == 0);
+
+    map_set(&map, "hoge", "HELLO");
+    char *value = map_get(map, "hoge");
+    assert(strcmp(value, "HELLO") == 0);
+
+    /* map_get */
+    value = map_get(map, "hello");
+    assert(strcmp(value, "world") == 0);
+
+    t_item *itr = map->item_head;
+    /*while (itr)
+    {
+        printf("%s\n", itr->name);
+        itr = itr->next;
+    }*/
+
+    /* map_unset */
+    map_set(&map, "FOO", "BAR");
+    map_unset(&map, "hello");
+    value = map_get(map, "hello");
+    // printf("%s\n", value);
+    assert(value == NULL);
+    assert(strcmp(map->item_head->name, "hoge") == 0);
+    assert(strcmp(map->item_head->next->name, "FOO") == 0);
+    // printf("%s\n", map->item_head->next->name);
+
+    // 先頭をunset
+    map_unset(&map, "hoge");
+
+    /*itr = map->item_head;
+    while (itr)
+    {
+        printf("%s\n", itr->name);
+        itr = itr->next;
+    }*/
+}
+#endif
