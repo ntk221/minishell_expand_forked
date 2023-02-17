dbe00dec93cc438e5474c00dc043c8f1add3f8e5
Sakata Atushi <satushi@c1r27s1.42tokyo.jp>
2023-02-18 00:53:57 +0900

    expand recover

--- expand/expand.c
+++ expand/expand.c
@@ -6,7 +6,7 @@
 /*   By: satushi <satushi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 00:37:31 by satushi           #+#    #+#             */
-/*   Updated: 2023/02/18 00:38:01 by satushi          ###   ########.fr       */
+/*   Updated: 2023/02/18 00:53:40 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,53 +32,46 @@ void append_char(char **s, char c)
 	*s = new;
 }
 
-void	quote_removal(t_token *tok)
-{
-	char *new_word;
-	char *p;
-
-	if (tok == NULL || tok->kind != TK_WORD || tok->word == NULL)
-		return ;
-	p = tok->word;
-	new_word = NULL;
-	while (*p && !is_metacharactert(*p))
-	{
-		if (*p == '\'')
-		{
-			p++;
-			while (*p != '\'')
-			{
-				if (*p == '\0')
-					fatal_error("TODO:single quote\n");
-				append_char(&new_word, *p++);
-			}
-			p++;
-		}
-		else if (*p == '\"')
-		{
-			p++;
-			while (*p != '\"')
-			{
-				if (*p == '\0')
-					fatal_error("TODO:double quote\n");
-				append_char(&new_word, *p++);
-			}
-			p++;
-		}
-		else
-			append_char(&new_word, *p++);
-	}
-	free(tok->word);
-	tok->word = new_word;
-	quote_removal(tok->next);
-}
-////////////
-
-// while (*p != '\'')
+// void	quote_removal(t_token *tok)
 // {
-// 	if (*p == '\0')
-// 		fatal_error("TODO:single quote\n");
-// 	append_char(&new_word, *p++);
+// 	char *new_word;
+// 	char *p;
+
+// 	if (tok == NULL || tok->kind != TK_WORD || tok->word == NULL)
+// 		return ;
+// 	p = tok->word;
+// 	new_word = NULL;
+// 	while (*p && !is_metacharactert(*p))
+// 	{
+// 		if (*p == '\'')
+// 		{
+// 			p++;
+// 			while (*p != '\'')
+//                 append_char(&new_word, *p++);
+// 			// {
+// 			// 	if (*p == '\0')
+// 			// 		fatal_error("TODO:single quote\n");
+// 			// }
+// 			p++;
+// 		}
+// 		else if (*p == '\"')
+// 		{
+// 			p++;
+// 			while (*p != '\"')
+//                 append_char(&new_word, *p++);
+// 			// {
+// 			// 	if (*p == '\0')
+// 			// 		fatal_error("TODO:double quote\n");
+// 			// 	append_char(&new_word, *p++);
+// 			// }
+// 			p++;
+// 		}
+// 		else
+// 			append_char(&new_word, *p++);
+// 	}
+// 	free(tok->word);
+// 	tok->word = new_word;
+// 	quote_removal(tok->next);
 // }
 
 void	expand_doller(char **dst, char **rest, char *p)
