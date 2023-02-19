2097fc18d12467e38ba65dcaae43b0f9418a6f47
molhot <sakata19991214@gmail.com>
2023-02-19 20:12:46 +0900

    push

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: satushi <satushi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 05:42:10 by satushi           #+#    #+#             */
-/*   Updated: 2023/02/19 20:06:52 by satushi          ###   ########.fr       */
+/*   Updated: 2023/02/19 20:12:39 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,21 +41,31 @@ t_token	*tokenizer(char *line)
 	{
 		if (consume_blank(&line, line))
 			continue ;
-		else if (is_operator(line))
+		else if (is_operator(line) || is_word(line) || is_redirect(line))
 		{
-			tok->next = operator(&line, line);
+			if (is_operator(line))
+				tok->next = operator(&line, line);
+			else if (is_word(line))
+				tok->next = word(&line, line);
+			else if (is_redirect(line))
+				tok->next = redirect(&line, line);
 			tok = tok->next;
 		}
-		else if (is_word(line))
-		{
-			tok->next = word(&line, line);
-			tok = tok->next;
-		}
-		else if (is_redirect(line))
-		{
-			tok->next = redirect(&line, line);
-			tok = tok->next;
-		}
+		// else if (is_operator(line))
+		// {
+		// 	tok->next = operator(&line, line);
+		// 	//tok = tok->next;
+		// }
+		// else if (is_word(line))
+		// {
+		// 	tok->next = word(&line, line);
+		// 	//tok = tok->next;
+		// }
+		// else if (is_redirect(line))
+		// {
+		// 	tok->next = redirect(&line, line);
+		// 	//tok = tok->next;
+		// }
 		else
 			tokenize_error("Unexpected Token", &line, line);
 	}
